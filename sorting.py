406ecfab3a46c90086ac0070110a10d9e4d688f3
valam <valamit@avccengg.net>
2022-10-17 13:58:51 +0530

    first commit

---
+++ sorting.py
@@ -0,0 +1,14 @@
+def bubble_sort(alist):
+    for i in range(len(alist)-1,0,1):
+        no_swap=True
+        for j in range(0,i):
+            if alist[j+1]<alist[j]:
+                alist[j].alist[j+1]=alist[j+1],alist[j]
+                no_swap=False
+            if no_swap:
+                return
+alist=input('enter the list of numbers:').split()
+alist=[int(x)for x in alist]
+bubble_sort(alist)
+print('sorted list:',end='')
+print(alist)
